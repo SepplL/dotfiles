05bb656271c087048a6352316721b7535f1956d1
Sebastian Lütge <sebastian.luetge@tu-dortmund.de>
2025-10-20 13:32:51 +0200

    start working on waybars

--- .dotfiles/.config/mango/autostart.sh
+++ .dotfiles/.config/mango/autostart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-waybar -c ~/.config/waybar/config-mango >/dev/null 2>&1 &
+dunst >/dev/null 2>&1 &
+waybar -c ~/.config/waybar/config-mangowc.jsonc >/dev/null 2>&1 &
 
 swayidle -w \
 	timeout 300 'swaylock -i ~/wallpaper/blockwavedawn.png' \
@@ -10,6 +11,3 @@ swayidle -w \
 
 # swaybg
 [[ -e ~/wallpaper/dracula-soft-waves-44475a.png ]] && swaybg -m fill -i ~/wallpaper/leafy-dawn.png >/dev/null 2>&1 &
-
-dunst >/dev/null 2>&1 &
-
